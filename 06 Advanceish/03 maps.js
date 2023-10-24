5eb52592a1696732d4d3f1106ff405fa17eb3885
Sambit Mohanty <sambit@localhost.localdomain>
2023-10-24 21:52:01 +0530

    maps

---
+++ 06 Advanceish/03 maps.js
@@ -0,0 +1,17 @@
+var myMaps = new Map();
+
+myMaps.set(0,"one");
+myMaps.set(1,"02");
+myMaps.set(2,"03");
+myMaps.set(3,"04");
+myMaps.set(4,"05");
+// console.log(myMaps);
+
+for (let key of myMaps.keys()) {        //for-of prefers keys as output. but budge when given anything else
+    //console.log(`key is ${key}`);
+}
+for (let [key,value] of myMaps) {
+    console.log(`key is ${key} and value is ${value}`);
+}
+
+myMaps.forEach ((value,key) => console.log(`value is ${value} and key is ${key}`));     //foreach returns the values.whaever the argument is provide a single argument
