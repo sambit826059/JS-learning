8cb83fd8c921fa07761bb2a8e8bb2e0b50def801
Sambit Mohanty <sambit@localhost.localdomain>
2023-10-24 19:17:16 +0530

    binding

---
+++ 05 Not Advanced/06 thisBinding.js
@@ -0,0 +1,29 @@
+const sam = {
+    firstName: "Sambit",
+    lastName: "Mohanty",
+    role: "admin",
+    courseCount: 5,
+    getInfo: function(){
+        console.log(`
+        First name is ${this.firstName}
+        Last name is ${this.lastName}
+        His role is ${this.role}
+        and he is studying ${this.courseCount} courses
+        `);
+    },
+};
+
+const dj = {
+    firstName: "That guy",
+    lastName: "Rock",
+    role: "sub-admin",
+    courseCount: 2,
+};
+
+// sam.getInfo();
+// dj.getInfo();
+
+var djInfo = sam.getInfo.bind(dj);  //bind gives refernce. we have to call it to log it
+// djInfo();
+
+sam.getInfo.call(dj);               //call directly logs the properties
