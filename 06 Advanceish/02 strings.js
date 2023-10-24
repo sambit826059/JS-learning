e36f43583df6bbf7f0ee17127479490fe5705170
Sambit Mohanty <sambit@localhost.localdomain>
2023-10-24 21:52:40 +0530

    strings

---
+++ 06 Advanceish/02 strings.js
@@ -0,0 +1,8 @@
+var firstName = `Sambit`;   // '' are generally used for single characters
+var fullName = `${firstName} Mohanty`;
+
+console.log(firstName);
+console.log(fullName.charAt(0));    //() used in strings . [] for arrays
+console.log(fullName.includes("Sambit"));
+console.log(firstName);
+console.log(fullName.toUpperCase());
