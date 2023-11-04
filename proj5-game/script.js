a2a751e32029116bb0a0a4c08a333bd80ed3109b
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-04 10:27:45 +0530

    #

---
+++ proj5-game/script.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+
+cards.forEach((card) => card.addEventListener("click", flip));
+
+
+
+var flip = () => {
+    console.log("Card Flipped");
+}
+
+var sucess = () => {
+    
+}
+
+var fail = () => {
+    
+}
+
+var reset = () => {
+    
+}
