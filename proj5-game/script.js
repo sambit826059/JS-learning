e46ee8c839b2c5ba91a4170ffc77ce1cbd06b872
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-12 10:05:08 +0530

    javascript code

--- proj5-game/script.js
+++ proj5-game/script.js
@@ -1,22 +1,64 @@
 const cards = document.querySelectorAll(".card");
-console.log(cards);
+// console.log(cards);
+
+//variables
+var isFlipped = false;
+var firstCard;
+var secondCard;
 
 cards.forEach((card) => card.addEventListener("click", flip));
 
 
 
-var flip = () => {
-    console.log("Card Flipped");
+function flip () {
+    // console.log("Card Flipped"); 
+    // console.log(this);
+    this.classList.add("flip");
+    if (!isFlipped) {
+        isFlipped = true;
+        firstCard = this;
+    }else {
+        secondCard = this;
+        console.log(firstCard);
+        console.log(secondCard);
+        checkIt();
+    }
+}
+// if arrow function is used, 'this' will be inherited from the surrounding scope
+
+function checkIt() {
+    // console.log("checking");
+    firstCard.dataset.image === secondCard.dataset.image ? success() : fail();
 }
 
-var sucess = () => {
+function success()  {
+    // console.log("success");
+    
+    firstCard.removeEventListener("click", flip);
+    secondCard.removeEventListener("click", flip);
+    reset();
+}
+
+function fail() {
+    // console.log("fail");
+    setTimeout ( () => {
+    firstCard.classList.remove("flip");     //here firstCard.this.classList.remove() is wrong if used .refer to flip method
+    secondCard.classList.remove("flip");
+    }, 1000);
     
 }
 
-var fail = () => {
-    
+function reset() {
+    isFlipped = false;
+    firstCard = null;     
+    secondCard = null;
 }
+//assignment-write reset method again with optimized code or logic e.g.destructuring the value, spread operator etc.
 
-var reset = () => {
-    
-}
+(function shuffle() {
+    cards.forEach((card) => {
+        var index = Math.floor(Math.random()*8)
+        card.style.order = index;
+    })
+})();
+
